b1e5cd5b27646ed1bb3e27fe9217efead4dd88a4
Behruz01 <ibragimovbehruz822@gmail.com>
2023-01-07 18:19:49 +0500

    changed

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ elForm.addEventListener("submit", function (evt) {
   const data = evt.target.data.value;
 
   const newElement = document.createElement("li");
-
+  const newArray = [];
+  newArray.push(newElement);
+  console.log(newArray);
   if (image !== "" && title !== "" && discription !== "" && data !== "") {
     newElement.className = "li-item";
     newElement.innerHTML += `
-            <img src="https://i.picsum.photos/id/701/200/300.jpg?hmac=gVWdD9Rh_J0iGXpXOJAN7MZpGPrpeHX_M5JwGGvTSsI" alt="${title}"/>
-            <div class="cards-div">
+            <img src="httards-div">
               <h3 class="item-h3">
                ${title}
               </h3>
-              <p class="item-p">
+              <p class="itemps://i.picsum.photos/id/701/200/300.jpg?hmac=gVWdD9Rh_J0iGXpXOJAN7MZpGPrpeHX_M5JwGGvTSsI" alt="${title}"/>
+            <div class="c-p">
                 ${discription}
               </p>  
               <p class="time">${data}</p>
